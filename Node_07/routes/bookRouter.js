fd42d7c3e497f109f5b14ac9f542ca7433677842
kol2005 <kol2005@naver.com>
2020-03-09 16:17:17 +0900

    2020-03-09

--- Node_07/routes/bookRouter.js
+++ Node_07/routes/bookRouter.js
@@ -1,43 +1,41 @@
-// express framework 를 사용한 router 생성
 var express = require("express")
 var router = express.Router()
-
 var bookVO = require("../models/bookVO")
 
-// selectAll 전체리스트
 router.get("/",function(req,res){
     bookVO.find({},function(err,books){
         res.render("book/list",{books:books})
     })
 })
-// 추가(insert) 화면 보여주기
+
 router.get("/insert",function(req,res){
     var book = new bookVO()
     res.render("book/write",{book:book,formTitle:'INSERT'})
 })
-// 추가 화면에서 저장 버튼을 클릭했을때
+
 router.post("/insert",function(req,res){
     var book = new bookVO(req.body)
     book.save(function(err,data){
         res.redirect("/book")
     })
 })
-// 변경,수정(update) 화면 보여주기
+
 router.get("/update/:id",function(req,res){
-    bookVO.findOne({_id:req.params.id},function(err,book){
+    var id = req.params.id
+    bookVO.findOne({_id:id},function(err,book){
         res.render("book/write",{book:book,formTitle:'UPDATE'})
     })
 })
-// 수정 화면에서 저장 버튼을 클릭했을때
+
 router.post("/update/:id",function(req,res){
-    let id = req.params.id
-    bookVO.update({_id:id},{$set:req.body},function(err,book){
-        res.redirect('/book')
+    var id = req.params.id
+    bookVO.update({_id:id},{$set:req.body},function(err,data){
+        res.redirect("/book")
     })
 })
-// 삭제
+
 router.get("/delete/:id",function(req,res){
-    let id = req.params.id
+    var id = req.params.id
     bookVO.deleteOne({_id:id},function(err,data){
         res.redirect("/book")
     })
