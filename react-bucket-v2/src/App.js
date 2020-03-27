77aeb27854aa91728ab52acf322f96ee0c4ea546
kol2005 <kol2005@naver.com>
2020-03-27 10:05:47 +0900

    2020-03-27

---
+++ react-bucket-v2/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import BucketMain from "./main/BucketMain";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>마이 버킷 리스트</h2>
+      </header>
+      <BucketMain />
+    </div>
+  );
+}
+
+export default App;
