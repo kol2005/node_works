d3b52f99d62a5a7bbff642a55ae0497e37d48f50
kol2005 <kol2005@naver.com>
2020-06-15 12:51:13 +0900

    2020-06-15

---
+++ Node_firebase_deploy/functions/Deploy.md
@@ -0,0 +1,28 @@
+# firebase Hosting 에 nodejs 프로젝트 올리기
+
+## 작업폴더 다시 작성
+
+- routes, views, config 폴더를 모두 functions 폴더로 이동
+- app.js 파일을 functions 폴더로 이동 후 index.js로 변경
+
+## functions 폴더에서 dependency 설정
+
+- npm install pug
+- npm install firebase
+- npm install firebase-functions
+- npm install moment
+- npm install moment-timezone
+- 인스톨 다 하고 마지막으로
+- npm install
+
+## functions 폴더의 package.json 수정
+
+- firebase 에서는 node 10까지 지원
+- "engines" : {
+  "nede" : "10"
+  }
+
+## project 폴더의 firebase.json 수정
+
+- rewrites의 destination을 삭제하고
+- "function" : "application" 추가
---
+++ Node_firebase_deploy/functions/index.js
@@ -0,0 +1,60 @@
+// var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
+
+var firebaseFunction = require("firebase-functions");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var bbsRouter = require("./routes/bbsRouter");
+
+var app = express();
+
+// view engine setup
+// pug template를 firebase Hosting에서 사용할수 있도록 설정
+app.engine("pug", require("pug").__express);
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "pug");
+
+// app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/bbs", bbsRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+/*
+firebase 에 nodejs 프로젝트를 deploy 하기 위해서
+app.js functions/index.js로 이동하고
+module.exports = app 대신
+exports.application = firebaseFunction.https.onRequest(app); 으로 변경
+exports 하는 application 이름은
+프로젝트 root firebase.json에 명시해 줘야한다
+
+index.js에는 꼭 필요한 미들웨어만 남기고
+나머지는 삭제해주는 것이 좋다.
+불필요한 미들웨어가 많으면 호환성 등의 문제를 일으키기 쉽다
+*/
+// module.exports = app;
+exports.application = firebaseFunction.https.onRequest(app);
